7d5a0b7b56da3da73641b22740459aeba3e134ac
Brandon Geren <brandongeren@gmail.com>
2017-03-15 18:00:25 -0500

    Added a necessary .extend to answer.js

--- Code/emberApp/app/models/answer.js
+++ Code/emberApp/app/models/answer.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
-export default DS.Model({
+export default DS.Model.extend({
     answerText: DS.attr('string'),
     dateOfAnswer: DS.attr('date'),
     answerRating: DS.attr('number')
