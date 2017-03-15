28e0ca27cf9c6f0e77fc83ff4af709c2c343d676
Brandon Geren <brandongeren@gmail.com>
2017-03-15 18:52:29 -0500

    Courses now display from the local database

--- Code/emberApp/app/models/course.js
+++ Code/emberApp/app/models/course.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    courseNumber: DS.attr('number'),
-    courseName: DS.attr('string')
+    number: DS.attr('number'),
+    name: DS.attr('string')
 });
