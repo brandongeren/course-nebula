c22277e42020696f5a7b9a40c755c819574f5d3e
Brandon Geren <brandongeren@gmail.com>
2017-03-17 03:59:42 -0500

    Added a link back to the detail page from the ask page

--- Code/sails/emberApp/app/routes/courses/show.js
+++ Code/sails/emberApp/app/routes/courses/show.js
@@ -1,13 +1,11 @@
 import Ember from 'ember';
 
-	var paramaters;
 
 export default Ember.Route.extend({
 	model(params) {
-		parameters = params;
 		return this.get('store').findRecord('course', params.course_id);
 	},
 	questions() {
-		return this.get('store').findAll('question', {course_id: params.course_id});
+		return this.get('store').findAll('question');
 	}
 });
