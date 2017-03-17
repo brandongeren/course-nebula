855e70518258d8023e33e7f84f5fa523b1f89a18
Brandon Geren <brandongeren@gmail.com>
2017-03-17 04:37:29 -0500

    Added stubs for question controller

--- Code/sails/emberApp/app/routes/courses/show/ask.js
+++ Code/sails/emberApp/app/routes/courses/show/ask.js
@@ -1,14 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	actions: {
-		savequestion: function(body) {
-			this.get('store').createRecord('question', {
-				text: body,
-				course: null,
-				date: new Date(),
-				rating: 0
-			});
-		}
-	}
+	
 });
