32c42b2e29d5843f94569032435ded439d46dbde
Brandon Geren <brandongeren@gmail.com>
2017-03-17 10:09:42 -0500

    fix question

--- Code/sails/emberApp/app/models/question.js
+++ Code/sails/emberApp/app/models/question.js
@@ -2,16 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     course: DS.belongsTo('course'),
-<<<<<<< HEAD
-    questionText: DS.attr('string'),
-    dateOfQuestion: DS.attr('date'),
-    questionRating: DS.attr('number'),
-});
-=======
     text: DS.attr('string'),
     date: DS.attr('date'),
     rating: DS.attr('number'),
 });
-
-
->>>>>>> 307aee394e6fa919099158cccd7b8a94ea095f2a
