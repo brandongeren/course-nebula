d55d797bfa2012ed1b537d15fa5b97834fb29158
mshanahan <michael.shanahan5@gmail.com>
2017-03-16 19:41:46 -0500

    database write done

--- Code/sails/server/api/controllers/UserController.js
+++ Code/sails/server/api/controllers/UserController.js
@@ -6,6 +6,32 @@
  */
 
 module.exports = {
-	
+    add: function(req,res) {
+         //{"UserID":"0","Username":"test","password":"pass","email":"me@gmail.com","userAnsweredQuestions":0};
+	var Username = req.param('Username');
+	var password = req.param('password');
+	var email = req.param('email');
+	var userAnsweredQuestions = req.param('userAnsweredQuestions');
+
+	var user = {
+	    Username : Username,
+	    password : password,
+	    email : email,
+	    userAnsweredQuestions : userAnsweredQuestions
+	}
+
+	User.create(user).exec(function(err,result) {
+	    if (err) {
+		sails.log.debug('some error occurred' + err);
+		return res.json(500, { error: 'some error occurred' });
+
+	    }
+
+	    sails.log.debug('Success',JSON.stringify(result));
+	    return res.json(200, {success: 'Success'});
+
+	});
+
+    }
 };
 
