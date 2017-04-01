4982b22760cbe87564f700def3287d79a36d92fc
Brandon Geren <brandongeren@gmail.com>
2017-03-31 20:05:38 -0500

    Deleted emberApp

--- Code/sails/emberApp/app/adapters/application.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-
-export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  authorizer: 'authorizer:oauth2',
-  namespace: 'api'
-});
