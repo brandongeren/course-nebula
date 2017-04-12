8008f19429bda92af20a2abf5a4a52283e231910
Tanner <hey@tannermarino.com>
2017-04-12 16:08:18 -0500

    remove unneeded code

--- Code/sails/OLDCLIENT/app/models/instructor.js
+++
@@ -1,6 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.model.extend({
-    user: DS.belongsTo('user'),
-    course: DS.belongsTo('course')
-});
--- Code/sails/client/tests/integration/components/courses-filter-test.js
+++ Code/sails/client/tests/integration/components/courses-filter-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('course-listing', 'Integration | Component | course listing', {
+moduleForComponent('courses-filter', 'Integration | Component | courses filter', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{course-listing}}`);
+  this.render(hbs`{{courses-filter}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#course-listing}}
+    {{#courses-filter}}
       template block text
-    {{/course-listing}}
+    {{/courses-filter}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
