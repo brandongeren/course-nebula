42f7dc407974af464a6f4b6150f94cebfdf065cd
mshanahan <michael.shanahan5@gmail.com>
2017-04-12 15:47:48 -0500

    feedback writes to backend and displays on frontend

--- Code/sails/client/app/controllers/courses/show/feedback.js
+++ Code/sails/client/app/controllers/courses/show/feedback.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    actions: {
+	savefeedback: function() {
+			var text = this.get('text');
+			var date = new Date();
+		        var course = this.get('model');
+			var rating = 1;
+			var feedback = this.get('store').createRecord('feedback', {
+				text: text,
+				date: date,
+			        courseN: this.get('model'),
+				rating: rating
+			});
+
+			this.set('text', '');
+			this.set('course', '');
+			feedback.save();
+			this.transitionToRoute('courses');
+		}
+	}
 });
