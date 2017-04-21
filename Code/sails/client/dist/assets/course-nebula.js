892bf043f4ccf77bff410c3b4e8d19081baf9472
mshanahan <michael.shanahan5@gmail.com>
2017-04-21 16:46:54 -0500

    course numbers support letters

--- Code/sails/client/dist/assets/course-nebula.js
+++ Code/sails/client/dist/assets/course-nebula.js
@@ -2183,7 +2183,7 @@ catch(err) {
 /* jshint ignore:start */
 
 if (!runningTests) {
-  require("course-nebula/app")["default"].create({"contentSecurityPolicy":{"default-src":"'none'","script-src":"'self' 'unsafe-inline' 'unsafe-eval'","font-src":"'self'","connect-src":"'self'","img-src":"'self'","report-uri":"'localhost'","style-src":"'self' 'unsafe-inline'","frame-src":"'none'"},"name":"course-nebula","version":"0.0.0+d82e4c55"});
+  require("course-nebula/app")["default"].create({"contentSecurityPolicy":{"default-src":"'none'","script-src":"'self' 'unsafe-inline' 'unsafe-eval'","font-src":"'self'","connect-src":"'self'","img-src":"'self'","report-uri":"'localhost'","style-src":"'self' 'unsafe-inline'","frame-src":"'none'"},"name":"course-nebula","version":"0.0.0+3c207cf6"});
 }
 
 /* jshint ignore:end */
